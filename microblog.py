d078d36ab4c47ecf4a2e2507ef6c9b2473254b6f
Barni-Pack <fiji8800@gmail.com>
2021-11-08 20:23:55 +0300

    5 Lesson

--- microblog.py
+++ microblog.py
@@ -4,4 +4,6 @@ from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
+    # Add this code to venv/activate.ps1 if using powershell
+    # $env:FLASK_APP = 'microblog.py'
     return {'db': db, 'User': User, 'Post': Post}
