4eb6e776f5af01f5e3016616ab8432370ba8d811
Barni-Pack <fiji8800@gmail.com>
2021-11-12 11:29:17 +0300

    Lesson 9

--- config.py
+++ config.py
@@ -16,3 +16,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ["fiji8800@gmail.com"]
+    
+    POSTS_PER_PAGE = 3
