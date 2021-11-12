96139caef1a3b606b80851150703db374c99c3a3
Barni-Pack <fiji8800@gmail.com>
2021-11-12 14:02:33 +0300

    Lesson 11

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 login = LoginManager(app)
 login.login_view = 'login'
-
+login.login_message='Please log in to access this page.'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
